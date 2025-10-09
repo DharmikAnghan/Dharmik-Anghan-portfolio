078d3a33ecc985052e381d64ffcec5ec3cc609da
DharmikAnghan <129717818+DharmikAnghan@users.noreply.github.com>
2025-10-09 09:51:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -105,3 +105,17 @@ dropdownMenuLinks.forEach(link => {
   });
 });
 
+
+document.getElementById('imageInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const img = document.getElementById('uploadedImage');
+            img.src = e.target.result;
+            img.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
